33ffe7a4dfa8af9323e20c63290d5778b329dc06
haringma15 <martin.haring2@edu.fh-joanneum.at>
2024-10-01 22:53:46 +0200

    Add button to arduino

--- Arduino/arduino_mqtt_sketch/arduino_mqtt_sketch.ino
+++ Arduino/arduino_mqtt_sketch/arduino_mqtt_sketch.ino
@@ -1,6 +1,8 @@
 // LED pins
 const int redLedPin = 12;
 const int greenLedPin = 13;
+const int buttonPin = 11;
+bool mqttConnected = false;
 
 void setup() {
   // Start serial communication at 9600 baud rate
@@ -9,6 +11,7 @@ void setup() {
   // Initialize LEDs
   pinMode(redLedPin, OUTPUT);
   pinMode(greenLedPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
 
   digitalWrite(redLedPin, LOW);
   digitalWrite(greenLedPin, LOW);
@@ -18,16 +21,32 @@ void setup() {
 
 void loop() {
   // Request MQTT connection via PC
-  Serial.println("CONNECT_MQTT");
+  if (!mqttConnected) {
+    Serial.println("CONNECT_MQTT");
 
-  // Wait for response
-  if (Serial.available() > 0) {
-    String response = Serial.readStringUntil('\n');
+    // Wait for response
+    if (Serial.available() > 0) {
+      String response = Serial.readStringUntil('\n');
 
-    if (response == "MQTT_CONNECTED") {
-      digitalWrite(greenLedPin, HIGH);
+      if (response == "MQTT_CONNECTED") {
+        mqttConnected = true;
+        digitalWrite(greenLedPin, HIGH);
+        delay(2000);
+        digitalWrite(greenLedPin, LOW);
+      }
+    } else {
+      digitalWrite(redLedPin, HIGH);
+      delay(200);
+      digitalWrite(redLedPin, LOW);
     }
   }
 
-  delay(30000);
+  if (mqttConnected && digitalRead(buttonPin) == HIGH) {
+    Serial.println("PUBLISH_MQTT");
+    digitalWrite(redLedPin, HIGH);
+    delay(1000);
+    digitalWrite(redLedPin, LOW);
+  }
+
+  delay(5000);
 }
--- Arduino/mqtt_serial_bridge.py
+++ Arduino/mqtt_serial_bridge.py
@@ -10,18 +10,15 @@ MQTT_PASSWORD = "ss2021"
 MQTT_TOPIC = "Arduino-Unity-MQTT-Test"
 
 # Arduino Initialization
-connected = False
 ser = serial.Serial('COM4', 9600)
 time.sleep(2)
 
 # Connect callback
 def on_connect(client, userdata, flags, rc):
-    global connected
     if rc == 0:
         print("Connected to MQTT broker")
         ser.write(b'MQTT_CONNECTED\n')
         client.subscribe(MQTT_TOPIC)
-        connected = True
     else:
         print(f"Failed to connect, return code {rc}")
         ser.write(b'MQTT_FAILED\n')
@@ -46,12 +43,14 @@ def connect_to_mqtt():
 
 # Serial communication loop
 while True:
-    if not connected:
-        if ser.in_waiting > 0:
-            message = ser.readline().decode('utf-8').strip()
+    if ser.in_waiting > 0:
+        message = ser.readline().decode('utf-8').strip()
+        
+        if message == "CONNECT_MQTT":
+            print("Attempting to connect to MQTT server...")
+            success = connect_to_mqtt()
+        
+        if message == "PUBLISH_MQTT":
+            print("Publish!")
             
-            if message == "CONNECT_MQTT":
-                print("Attempting to connect to MQTT server...")
-                success = connect_to_mqtt()
-                
-        time.sleep(1)
+    time.sleep(1)
